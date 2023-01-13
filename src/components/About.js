af3955c03b146cad93b4dbc0cdad801c0002cc21
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-13 21:33:18 +0530

    Added Dark Mode Button for About Page Accordian

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,32 @@
+import React, { useState } from "react";
 import { Container, Typography } from "@mui/joy";
-import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
-import React from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+} from "@mui/material";
 
 function About() {
+  const [mode, setMode] = useState({
+    backgroundColor: "#fff",
+    color: "#000",
+  });
+
+  function changeMode() {
+    if (mode.color === "#000") {
+      setMode({
+        backgroundColor: "#000",
+        color: "#fff",
+      });
+    } else {
+      setMode({
+        backgroundColor: "#fff",
+        color: "#000",
+      });
+    }
+  }
+
   return (
     <>
       <Container maxWidth="lg" sx={{ mt: "2rem" }}>
@@ -10,11 +34,15 @@ function About() {
         // expanded={expanded === "panel1"}
         // onChange={handleChange("panel1")}
         >
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography>Collapsible Group Item #1</Typography>
+          <AccordionSummary
+            aria-controls="panel1d-content"
+            id="panel1d-header"
+            style={mode}
+          >
+            <Typography style={mode}>Collapsible Group Item #1</Typography>
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
+          <AccordionDetails style={mode}>
+            <Typography style={mode}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
               eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -27,11 +55,15 @@ function About() {
         // expanded={expanded === "panel2"}
         // onChange={handleChange("panel2")}
         >
-          <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-            <Typography>Collapsible Group Item #2</Typography>
+          <AccordionSummary
+            aria-controls="panel2d-content"
+            id="panel2d-header"
+            style={mode}
+          >
+            <Typography style={mode}>Collapsible Group Item #2</Typography>
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
+          <AccordionDetails style={mode}>
+            <Typography style={mode}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
               eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -44,11 +76,15 @@ function About() {
         // expanded={expanded === "panel3"}
         // onChange={handleChange("panel3")}
         >
-          <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <AccordionSummary
+            aria-controls="panel3d-content"
+            id="panel3d-header"
+            style={mode}
+          >
             <Typography>Collapsible Group Item #3</Typography>
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
+          <AccordionDetails style={mode}>
+            <Typography style={mode}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
               eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -57,6 +93,18 @@ function About() {
             </Typography>
           </AccordionDetails>
         </Accordion>
+        <Button
+          sx={{
+            background: "#1976d2",
+            color: "white",
+            mt: "1rem",
+            mx: "0.2rem",
+            // transition: "none",
+          }}
+          onClick={changeMode}
+        >
+          Enable Dark Mode
+        </Button>
       </Container>
     </>
   );
