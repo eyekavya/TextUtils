05f177098331c5e69d54f7b45665f3b235aac478
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-14 17:57:12 +0530

    Added Switch on Nav for changing mode

--- src/App.js
+++ src/App.js
@@ -5,10 +5,30 @@ import Container from "@mui/material/Container";
 import About from "./components/About";
 import Heading from "./components/Heading";
 
+import { useState } from "react";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { palette } from "@mui/system";
+
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const dark = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+  const light = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={dark} />
+
       {/* <About /> */}
 
       <Container maxWidth="sm">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,40 +5,55 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import { ThemeProvider } from "@mui/material";
+import ModeSwitch from "./ModeSwitch";
 
 const pages = ["Home", "About"];
 
 function Navbar(props) {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            {props.title}
-          </Typography>
+    <ThemeProvider theme={props.mode}>
+      <AppBar position="static">
+        <Container
+          maxWidth="xl"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              {props.title}
+            </Typography>
 
-          {pages.map((page) => (
-            <Button key={page} sx={{ my: 2, color: "white", display: "block" }}>
-              {page}
-            </Button>
-          ))}
-        </Toolbar>
-      </Container>
-    </AppBar>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Toolbar>
+          <ModeSwitch />
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 }
 export default Navbar;
